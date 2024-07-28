14d940e8b36e421e3766735bafa8726c372d5ef1
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2024-07-28 22:02:58 +0300

    Add Navbar component with basic structure and links

---
+++ src/Components/Hero.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+const Hero = () => {
+return (
+    <>
+  <div className="mt-40 text-stone-700 ">
+        <h1 className="text-9xl ml-20 ">Jose <br /> Ocando</h1>
+        <div className="grid grid-cols-2  mt-10 ml-20">
+        <p className="text-3xl max-w-3xl ">One of my deepest ioys comes from from equipping cretive pepole with the visual tools they need to further their work</p>
+        
+    <ul>
+      <li ><Link href="#" className="flex mb-1 text-xl">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+</svg>
+<p className="ml-2">Learn fun faxts about me</p>
+      </Link></li>
+      <li><Link href="#" className="flex mb-1 text-xl">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+</svg>
+<p className="ml-2">Browse my work history</p>
+      </Link></li>
+      <li><Link href="#" className="flex mb-1 text-xl">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+</svg>
+<p className="ml-2">Read a featured case study</p>
+      </Link></li>
+      <li><Link href="#" className="flex mb-1 text-xl">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.994 15.994 0 0 1 1.622-3.395m3.42 3.42a15.995 15.995 0 0 0 4.764-4.648l3.876-5.814a1.151 1.151 0 0 0-1.597-1.597L14.146 6.32a15.996 15.996 0 0 0-4.649 4.763m3.42 3.42a6.776 6.776 0 0 0-3.42-3.42" />
+</svg>
+<p className="ml-2">Read a recent blog</p>
+      </Link></li>
+    </ul>
+  </div>
+  </div>
+  </>
+)
+}
+
+export default Hero;
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+
+const Navbar = () => {
+return (
+    <>
+    <nav className="flex justify-center items-center mt-6">
+        <ul className="flex justify-center items-center gap-40 w-7/12 bg-neutral-200 p-2 rounded-full  ">
+        <li>
+        <Link  href="#">
+        <svg 
+        xmlns="http://www.w3.org/2000/svg"
+         fill="none" viewBox="0 0 24 24" 
+         strokeWidth={1.5} stroke="currentColor"
+    className="h-6 w-6">
+  <path strokeLinecap="round"
+   strokeLinejoin="round"
+    d="M6.429 9.75 2.25 12l4.179 2.25m0-4.5 5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0 4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0-5.571 3-5.571-3" />
+</svg>
+        </Link>
+        </li>
+        <li><Link  href="#">Worg</Link></li>
+        <li><Link href="#">About</Link></li>
+        <li><Link href="#">Blog</Link></li>
+        <li><Link href="#">Contact</Link></li>
+    </ul>
+</nav>
+</>
+)
+}
+
+export default Navbar;
